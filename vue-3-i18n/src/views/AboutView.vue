0e1496ce1f2944f4ad1b93dfdb880cc05b621884
bodrovis <bodrovis@protonmail.com>
2023-01-14 19:05:24 +0200

    updates to vue

--- vue-3-i18n/src/views/AboutView.vue
+++ vue-3-i18n/src/views/AboutView.vue
@@ -2,6 +2,6 @@
   <div>
     <h1>{{ $t("about.header") }}</h1>
 
-    <p>{{ $t("about.donations") }} {{ $n(1456, "currencyFormat") }}</p>
+    <p>{{ $t("about.donations", { donations: $n(1456, "currencyFormat") }) }}</p>
   </div>
 </template>
